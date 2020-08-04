31d3793cb423c3c8549197568904f435b665146c
zyjswiadomieeu <maciejurmanski@gmail.com>
2020-08-04 15:09:31 +0200

    .

--- wegemoc/wsgi.py
+++ wegemoc/wsgi.py
@@ -1,16 +1,17 @@
 """
-WSGI config for wegemoc project.
+WSGI config for chwycmarzenia project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wegemoc.settings')
+
 application = get_wsgi_application()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wegemoc.settings')
