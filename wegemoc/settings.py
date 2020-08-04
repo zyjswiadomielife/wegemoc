c42a9f6a33e6fd107d359dbab7127093930a412b
zyjswiadomieeu <maciejurmanski@gmail.com>
2020-08-04 14:57:11 +0200

    .

--- wegemoc/settings.py
+++ wegemoc/settings.py
@@ -208,6 +208,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
