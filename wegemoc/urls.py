ccaa675c92c4a8c70ca257b3b994afee85f35eb0
zyjswiadomieeu <maciejurmanski@gmail.com>
2020-08-04 11:46:28 +0200

    statyczne

--- wegemoc/urls.py
+++ wegemoc/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, re_path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from home.views import home
 
 def trigger_error(request):
@@ -36,3 +37,5 @@ urlpatterns = [
     re_path(r"^messages/", include("pinax.messages.urls", namespace="pinax_messages")),
     path('sentry-debug/', trigger_error),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
