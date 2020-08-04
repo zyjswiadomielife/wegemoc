75c7d04aeef3b31588b91d897877f158c3621fb3
zyjswiadomieeu <maciejurmanski@gmail.com>
2020-08-04 14:47:22 +0200

    .

--- wegemoc/settings.py
+++ wegemoc/settings.py
@@ -207,11 +207,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
